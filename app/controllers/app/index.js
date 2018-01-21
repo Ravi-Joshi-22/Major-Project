69040cf61b3917e5cc44b56f4cde3bd975eadada
Sanyog Sharma <sanyog96@gmail.com>
2018-01-21 23:36:23 +0530

    now possibly conflicts are resolved prototype 1

--- app/controllers/app/index.js
+++ app/controllers/app/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const companyRoutes = require('./company');
 const commonRoutes = require('./common');
 //const adminRoutes = require('./admin');
-//const intervieweeRoutes = require('./interviewee');
+const intervieweeRoutes = require('./interviewee');
 
 const router = express.Router();
 
 router.use('/', commonRoutes);
 router.use('/company', companyRoutes);
 //router.use('/admin', adminRoutes);
-//router.use('/interviewee', intervieweeRoutes);
+router.use('/interviewee', intervieweeRoutes);
 module.exports = router;
