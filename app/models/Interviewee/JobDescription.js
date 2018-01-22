8eceab662a543e7072d49687dc3b329a526c0230
Jo-ravar <ravijoshi0996@gmail.com>
2018-01-22 20:52:49 +0530

    solved constants error

--- app/models/Interviewee/JobDescription.js
+++ app/models/Interviewee/JobDescription.js
@@ -1,40 +1,38 @@
 const mongoose = require('mongoose');
-const CONSTANTS = require('../../config/constants');
+const CONSTANTS = require('../../../config/constants');
 
 const Schema = mongoose.Schema;
-const jobDescriptionSchema = new Schema(
-    {
-        profile: {
-            type: String,
-            required: true,
-        },
-        organization: {
-            type: String,
-            required: true,
-        },
-        location: {
-            type: String,
-            required: true,
-        },
-        start_date: {
-            type: String,
-            required: true,
-        },
-        currently_working: {
-            type: Boolean,
-        },
-        end_date: {
-            type: String,
-            required: function () {
-                return (!(this.currently_working == true));
-            },
-            default: Date.now(),
-        },
-        description: {
-            type: String,
-            maxlength: 300,
-        },
+const jobDescriptionSchema = new Schema({
+  profile: {
+    type: String,
+    required: true,
+  },
+  organization: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  start_date: {
+    type: String,
+    required: true,
+  },
+  currently_working: {
+    type: Boolean,
+  },
+  end_date: {
+    type: String,
+    required: function() {
+      return !(this.currently_working == true);
     },
-);
+    default: Date.now(),
+  },
+  description: {
+    type: String,
+    maxlength: 300,
+  },
+});
 
 module.exports = jobDescriptionSchema;
