f91f9fad297565eccb423f480bf8cba37268760a
shreyajhunjhunwala7@gmail.com <shivani3608@gmail.com>
2018-02-19 21:28:57 +0530

    Table changed

--- client/src/components/userDashboard/elligibleTable.js
+++ client/src/components/userDashboard/elligibleTable.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
-} from 'material-ui/Table';
 import RaisedButton from 'material-ui/RaisedButton/RaisedButton';
 
 class ElligibleTable extends React.Component {
@@ -18,54 +10,54 @@ class ElligibleTable extends React.Component {
   render() {
     return (
       <MuiThemeProvider>
-        <Table selectable={false}>
-          <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-            <TableRow>
-              <TableHeaderColumn style={{ width: '20%' }}>
+        <table>
+          <thead>
+            <tr>
+              <th style={{ width: '20%' }}>
                 LOCATION
-              </TableHeaderColumn>
-              <TableHeaderColumn style={{ width: '15%' }}>
+              </th>
+              <th style={{ width: '15%' }}>
                 EXPERIENCE
-              </TableHeaderColumn>
-              <TableHeaderColumn style={{ width: '20%' }}>
+              </th>
+              <th style={{ width: '20%' }}>
                 START DATE
-              </TableHeaderColumn>
-              <TableHeaderColumn style={{ width: '20%' }}>
+              </th>
+              <th style={{ width: '20%' }}>
                 END DATE
-              </TableHeaderColumn>
-              <TableHeaderColumn style={{ width: '15%' }}>
+              </th>
+              <th style={{ width: '15%' }}>
                 SALARY
-              </TableHeaderColumn>
-              <TableHeaderColumn style={{ width: '20%' }}>
+              </th>
+              <th style={{ width: '20%' }}>
                 APPLY
-              </TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody displayRowCheckbox={false}>
+              </th>
+            </tr>
+            </thead>
+          <tbody displayRowCheckbox={false}>
             {this.props.interviewData.map((eachInterview, key) => (
-              <TableRow>
-                <TableRowColumn style={{ width: '20%' }}>
+              <tr>
+                <td style={{ width: '20%' }}>
                   {eachInterview.location}
-                </TableRowColumn>
-                <TableRowColumn style={{ width: '15%' }}>
+                </td>
+                <td style={{ width: '15%' }}>
                   {eachInterview.experience}
-                </TableRowColumn>
-                <TableRowColumn style={{ width: '20%' }}>
+                </td>
+                <td style={{ width: '20%' }}>
                   {eachInterview.sDate}
-                </TableRowColumn>
-                <TableRowColumn style={{ width: '20%' }}>
+                </td>
+                <td style={{ width: '20%' }}>
                   {eachInterview.eDate}
-                </TableRowColumn>
-                <TableRowColumn style={{ width: '15%' }}>
+                </td>
+                <td style={{ width: '15%' }}>
                   {eachInterview.salary}
-                </TableRowColumn>
-                <TableRowColumn style={{ width: '20%' }}>
+                </td>
+                <td style={{ width: '20%' }}>
                     <RaisedButton label="APPLY" primary={true} />
-                </TableRowColumn>
-              </TableRow>
+                </td>
+              </tr>
             ))}
-          </TableBody>
-        </Table>
+          </tbody>
+        </table>
       </MuiThemeProvider>
     );
   }
