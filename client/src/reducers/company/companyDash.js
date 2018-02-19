6557d2c1b7c86cca6ae2f465e090a93d134ac9dc
saurabh2044 <24.sinha.shubham@gmail.com>
2018-02-20 03:21:03 +0530

    Added Company details to Dashboard

--- client/src/reducers/company/companyDash.js
+++ client/src/reducers/company/companyDash.js
@@ -1,8 +1,9 @@
-import { FETCH_COMPANY } from '../../actions/company/types';
+import { COMPANY_DASH } from '../../actions/company/types';
 
 export default function (state = null, action) {
     switch (action.type) {
-        case FETCH_COMPANY:
+       
+        case COMPANY_DASH:
             return action.payload || false;
         default:
             return state;
