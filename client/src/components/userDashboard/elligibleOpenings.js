f120089ef0ed62264d6effe258db2d7afe8d8d78
shreyajhunjhunwala7@gmail.com <shivani3608@gmail.com>
2018-02-19 20:52:45 +0530

    Changes

--- client/src/components/userDashboard/elligibleOpenings.js
+++ client/src/components/userDashboard/elligibleOpenings.js
@@ -3,6 +3,8 @@ import { Card, CardText } from "material-ui/Card";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import CardHeader from "material-ui/Card/CardHeader";
 import ElligibleTable from "./elligibleTable";
+import MoreIcon from 'material-ui/svg-icons/av/fast-forward';
+import FlatButton from 'material-ui/FlatButton';
 
 const AmdocsData = [
   {
@@ -80,6 +82,7 @@ class ElligibleOpenings extends React.Component {
           />
           <CardText style={{ padding: 2 }}>
             <ElligibleTable interviewData={ZensarData} />
+            <FlatButton label="MORE DETAILS" primary={true} icon={<MoreIcon/>}/>
           </CardText>
         </Card>
         <Card style={{ padding: 5, margin: 20}}>
@@ -90,6 +93,7 @@ class ElligibleOpenings extends React.Component {
           />
           <CardText style={{ padding: 2 }}>
           <ElligibleTable interviewData={TCSData} />
+          <FlatButton label="MORE DETAILS" primary={true} icon={<MoreIcon/>}/>
           </CardText>
         </Card>
         <Card style={{ padding: 5, margin: 20}}>
@@ -100,6 +104,7 @@ class ElligibleOpenings extends React.Component {
           />
           <CardText style={{ padding: 2 }}>
           <ElligibleTable interviewData={AmdocsData} />
+          <FlatButton label="MORE DETAILS" primary={true} icon={<MoreIcon/>}/>
           </CardText>
         </Card>
         <Card style={{ padding: 5, margin: 20}}>
@@ -110,6 +115,7 @@ class ElligibleOpenings extends React.Component {
           />
           <CardText style={{ padding: 2 }}>
           <ElligibleTable interviewData={AppleData} />
+          <FlatButton label="MORE DETAILS" primary={true} icon={<MoreIcon/>}/>
           </CardText>
         </Card>
         <Card style={{ padding: 5, margin: 20}}>
@@ -120,6 +126,7 @@ class ElligibleOpenings extends React.Component {
           />
           <CardText style={{ padding: 2 }}>
           <ElligibleTable interviewData={CapgeminiData} />
+          <FlatButton label="MORE DETAILS" primary={true} icon={<MoreIcon/>}/>
           </CardText>
         </Card>
         <Card style={{ padding: 5, margin: 20}}>
@@ -130,6 +137,7 @@ class ElligibleOpenings extends React.Component {
           />
           <CardText style={{ padding: 2 }}>
           <ElligibleTable interviewData={CognizantData} />
+          <FlatButton label="MORE DETAILS" primary={true} icon={<MoreIcon/>}/>
           </CardText>
         </Card>
       </MuiThemeProvider>
--- client/src/pages/userDashboard/index.js
+++ client/src/pages/userDashboard/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-import MetaTags from 'react-meta-tags';
 import UserDrawer from "../../components/userDashboard/userDrawer";
 import ElligibleTitle from "../../components/userDashboard/elligibleTitle";
 import PrefsCard from '../../components/userDashboard/preferencesCard';
 import FilterCard from '../../components/userDashboard/filterCard';
 import ElligibleOpenings from '../../components/userDashboard/elligibleOpenings';
 import Card from 'material-ui/Card';
-import Footer from '../../footer';
+import Footer from '../../components/common/footer';
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 class UserDashboard extends React.Component {
@@ -18,18 +17,12 @@ class UserDashboard extends React.Component {
     return (
       <MuiThemeProvider>
       <div>
-          <MetaTags>
-            <meta
-              name="viewport"
-              content="initial-scale=1.0, width=device-width"
-            />
-          </MetaTags>
         <div className="content">
         <UserDrawer />
         <div className="columns">
         <div className="column is-8">
-        <Card style={{ padding: 5, margin: 20 }}>
         <ElligibleTitle/>
+        <Card style={{ padding: 5, margin: 20 }}>
         <ElligibleOpenings/>
         </Card>
         </div>
