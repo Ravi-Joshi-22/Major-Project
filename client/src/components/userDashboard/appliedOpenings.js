741d9b6afb588f523b9f17c0c753b5c6af2db570
saurabh2044 <24.sinha.shubham@gmail.com>
2018-02-21 06:58:23 +0530

    Applied opening dialog done

--- client/src/components/userDashboard/appliedOpenings.js
+++ client/src/components/userDashboard/appliedOpenings.js
@@ -99,8 +99,8 @@ class AppliedOpenings extends React.Component {
   render() {
     return (
       <MuiThemeProvider>
-        <Card style={{ padding: 5, margin: 20 }}>
-          <CardTitle title="Your Applied Openings" />
+        <Card style={{ padding: 10, margin: 0 }}>
+          {/* <CardTitle title="Your Applied Openings" /> */}
           <CardText style={{ padding: 2 }}>
             <Subheader><font color='#00BCD4'>Pending Interviews</font></Subheader>
             <Divider/>
@@ -108,7 +108,7 @@ class AppliedOpenings extends React.Component {
             <Subheader><font color='#00BCD4'>Interviews Already Given</font></Subheader>
             <Divider/>
             <GivenTable interviewData={givenAppliedData} />
-            <FlatButton label="BACK" primary={true} icon={<BackIcon />} />
+            {/* <FlatButton label="BACK" primary={true} icon={<BackIcon />} /> */}
           </CardText>
         </Card>
       </MuiThemeProvider>
