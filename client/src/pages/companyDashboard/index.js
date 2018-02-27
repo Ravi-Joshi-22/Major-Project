c7deb8f7e045d5658ec14b1356de968ffccd374b
saurabh2044 <24.sinha.shubham@gmail.com>
2018-02-27 11:08:33 +0530

    Appbar

--- client/src/pages/companyDashboard/index.js
+++ client/src/pages/companyDashboard/index.js
@@ -2,14 +2,20 @@ import React from "react";
 import { connect } from "react-redux";
 import * as actions from "../../actions/company";
 import "./index.css";
-import Title from "../../components/companyDashboard/Title";
+// import Title from "../../components/companyDashboard/Title";
 import MainArea from "../../components/companyDashboard/MainArea";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import NavDrawer from "../../components/companyDashboard/NavDrawer";
 import InterviewOpening from "../../components/companyDashboard/DrawerArea/InterviewOpening";
 import Drawer from "material-ui/Drawer";
 import RaisedButton from "material-ui/RaisedButton";
-import FloatingActionButton from "material-ui/FloatingActionButton";
+import IconButton from "material-ui/IconButton";
+import IconMenu from "material-ui/IconMenu";
+import HamburgerIcon from "material-ui/svg-icons/navigation/menu";
+import AppBar from "material-ui/AppBar";
+import Payment from "../../components/companyDashboard/payment";
+import Avatar from "material-ui/Avatar";
+// import FloatingActionButton from "material-ui/FloatingActionButton";
 
 class CompanyDashboard extends React.Component {
   constructor(props) {
@@ -23,6 +29,14 @@ class CompanyDashboard extends React.Component {
   }
 
   handleToggle = () => this.setState({ open: !this.state.open });
+
+  renderContents() {
+    if (this.props.company) {
+      return this.props.company.credits;
+    } else {
+      return this.props.credits;
+    }
+  }
 
   async openingForm() {
     if (this.state.currentModalClass === "modal") {
@@ -77,34 +91,56 @@ class CompanyDashboard extends React.Component {
   }
 
   render() {
-    const fabOpenStyle = {
-      marginLeft: -80,
-      marginTop: 10
-    };
-    const blackiconcolor = {
-      color: "#4a4a4a"
-    };
     const contentStyle = {
-      marginTop: -60
+      marginTop: 50
     };
 
     return (
       <div>
         <MuiThemeProvider>
           <div>
-            <FloatingActionButton
-              onClick={this.handleToggle}
-              backgroundColor="white"
-              iconClassName="fa fa-bars"
-              iconStyle={blackiconcolor}
-              style={fabOpenStyle}
+            <AppBar
+              title="SmartHyre"
+              iconElementLeft={
+                <IconButton>
+                  <HamburgerIcon />
+                </IconButton>
+              }
+              iconElementRight={
+                <div className="control columns" style={{ margin: "auto"}}>
+                <div className="column-8">
+                  <span className="is-large" style={{ color: "#fff"}}>
+                    {this.renderTitle()} &nbsp;
+                    <Payment />
+                  </span>
+                  </div>
+                  <div className="navbar-item has-dropdown is-hoverable column-4">
+                    <a className="navbar-link" href="#">
+                      <Avatar src="./Assets/Employee.svg" size={30} />
+                    </a>
+                    <div className="navbar-dropdown">
+                      <a className="navbar-item" href="#">
+                        Profile
+                      </a>
+                      <hr className="navbar-divider" />
+                      <a className="navbar-item is-active" href="#">
+                        Logout
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              }
+              onLeftIconButtonClick={this.handleToggle}
+              onTitleClick={() => {
+                this.props.homeCallback();
+              }}
+              style={{ width: "114%", marginLeft: "-7%" }}
             />
             <div style={contentStyle}>
               <InterviewOpening
                 currentModalClass={this.state.currentModalClass}
                 openingCallback={this.openingForm}
               />
-              <Title credits={this.renderTitle()} />
               <Drawer
                 docked={false}
                 width={300}
