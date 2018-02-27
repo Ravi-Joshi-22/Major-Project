2b9c13adb5ff5957ded6b862062319f690aa936f
Sanyog Sharma <sanyog96@gmail.com>
2018-02-27 18:25:20 +0530

    Updation and Deleteion of all types of Education, addition of new Degrees

--- app/controllers/interviewee/profile.js
+++ app/controllers/interviewee/profile.js
@@ -24,6 +24,64 @@ function updateSecondaryDetails(req, res, next) {
 }
 
 /**
+ * Function to update Senior Secondary Details
+ * @param {Object} req Request Object
+ * @param {Object} res Response Object
+ * @param {Function} next Function to pass control to the next middleware
+ */
+function updateSeniorSecondaryDetails(req, res, next) {
+  profileLib.updateSeniorSecondaryOfInterviewee(
+    req.user._id,
+    req.body.senior_secondary,
+    function(errInUpdation, updatedInstance) {
+      if (errInUpdation) {
+        res.status(500).json(errInUpdation);
+      } else {
+        res.status(200).json(updatedInstance);
+      }
+    }
+  );
+}
+
+/**
+ * Function to delete Senior Secondary Details
+ * @param {Object} req Request Object
+ * @param {Object} res Response Object
+ * @param {Function} next Function to pass control to the next middleware
+ */
+function deleteSeniorSecondaryDetails(req, res, next) {
+  profileLib.deleteSeniorSecondaryOfInterviewee(req.user._id, function(
+    errInUpdation,
+    updatedInstance
+  ) {
+    if (errInUpdation) {
+      res.status(500).json(errInUpdation);
+    } else {
+      res.status(200).json(updatedInstance);
+    }
+  });
+}
+
+/**
+ * Function to add new Degree Details
+ * @param {Object} req Request Object
+ * @param {Object} res Response Object
+ * @param {Function} next Function to pass control to the next middleware
+ */
+function newDegreeDetails(req, res, next) {
+  profileLib.newDegreeOfInterviewee(req.user._id, req.body.degree, function(
+    errInUpdation,
+    updatedInstance
+  ) {
+    if (errInUpdation) {
+      res.status(500).json(errInUpdation);
+    } else {
+      res.status(200).json(updatedInstance);
+    }
+  });
+}
+
+/**
  * Function to update Degree Details
  * @param {Object} req Request Object
  * @param {Object} res Response Object
@@ -42,7 +100,30 @@ function updateDegreeDetails(req, res, next) {
   });
 }
 
+/**
+ * Function to delete Degree Details
+ * @param {Object} req Request Object
+ * @param {Object} res Response Object
+ * @param {Function} next Function to pass control to the next middleware
+ */
+function deleteDegreeDetails(req, res, next) {
+  profileLib.deleteDegreeOfInterviewee(req.user._id, req.body._id, function(
+    errInUpdation,
+    updatedInstance
+  ) {
+    if (errInUpdation) {
+      res.status(500).json(errInUpdation);
+    } else {
+      res.status(200).json(updatedInstance);
+    }
+  });
+}
+
 router.post('/updateSecondaryDetails', updateSecondaryDetails);
+router.post('/updateSeniorSecondaryDetails', updateSeniorSecondaryDetails);
+router.get('/deleteSeniorSecondaryDetails', deleteSeniorSecondaryDetails);
+router.post('/newDegreeDetails', newDegreeDetails);
 router.post('/updateDegreeDetails', updateDegreeDetails);
+router.post('/deleteDegreeDetails', deleteDegreeDetails);
 
 module.exports = router;
