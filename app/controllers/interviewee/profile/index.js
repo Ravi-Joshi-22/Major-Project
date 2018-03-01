bd342229f0bfd2fa2c86277befab6c61ce922e90
Sanyog Sharma <sanyog96@gmail.com>
2018-03-01 22:34:14 +0530

    additional, updation and deletion of additional information now can be performed of any Interviewee

--- app/controllers/interviewee/profile/index.js
+++ app/controllers/interviewee/profile/index.js
@@ -6,6 +6,7 @@ const professionRoutes = require('./profession');
 const testRoutes = require('./test');
 const certificationRoutes = require('./certification');
 const courseRoutes = require('./course');
+const additionalRoutes = require('./additional');
 
 const router = express.Router();
 
@@ -16,7 +17,7 @@ router.use('/profession', professionRoutes);
 router.use('/test', testRoutes);
 router.use('/certification', certificationRoutes);
 router.use('/course', courseRoutes);
-// router.use('/additional', additionalRoutes);
+router.use('/additional', additionalRoutes);
 // router.use('/skill', skillRoutes);
 // router.use('/project', projectRoutes);
 module.exports = router;
