54d077ccd859ea16c033b5a4f0fd6e3384543ff0
Sanyog Sharma <sanyog96@gmail.com>
2018-03-17 22:20:34 +0530

    changes for error reporting when null or empty objects are sent

--- app/controllers/interviewee/profile/project.js
+++ app/controllers/interviewee/profile/project.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const projectLib = require('../../../../lib/interviewee/profile/project');
+const CONSTANTS = require('../../../../config/constants');
 
 const router = express.Router();
 
@@ -10,16 +11,61 @@ const router = express.Router();
  * @param {Function} next Function to pass control to the next middleware
  */
 function addProjectDetails(req, res, next) {
-  projectLib.addProjectOfInterviewee(req.user._id, req.body.project, function(
-    errInUpdation,
-    updatedInstance
+  if (req.body.hasOwnProperty('project') == false) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert projects details for project data inside object of project property',
+      errorDetail: 'There exists no project property for usage',
+    };
+    res.status(500).json(err);
+  } else if (
+    req.body.project.hasOwnProperty('title') == false ||
+    req.body.project.hasOwnProperty('description') == false
   ) {
-    if (errInUpdation) {
-      res.status(500).json(errInUpdation);
-    } else {
-      res.status(200).json(updatedInstance);
-    }
-  });
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert a title and description field(required) inside requested Objects project property',
+      errorDetail:
+        'There exists no title or description or both property inside project object for usage',
+    };
+    res.status(500).json(err);
+  } else if (
+    req.body.project.title == '' ||
+    req.body.project.description == ''
+  ) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert a title and description field(required) inside requested Objects project property',
+      errorDetail:
+        'The title or description or both fields are empty (Insert a specific different title from other projects title and a brief specific description of your project)',
+    };
+    res.status(500).json(err);
+  } else if (
+    typeof req.body.project.description != 'string' ||
+    typeof req.body.project.title != 'string'
+  ) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg: 'Insert correct values for property to be stored',
+      errorDetail:
+        'There exists some incorrect value for some property inside object requested',
+    };
+    res.status(500).json(err);
+  } else {
+    projectLib.addProjectOfInterviewee(req.user._id, req.body.project, function(
+      errInUpdation,
+      updatedInstance
+    ) {
+      if (errInUpdation) {
+        res.status(500).json(errInUpdation);
+      } else {
+        res.status(200).json(updatedInstance);
+      }
+    });
+  }
 }
 
 /**
@@ -29,17 +75,79 @@ function addProjectDetails(req, res, next) {
  * @param {Function} next Function to pass control to the next middleware
  */
 function updateProjectDetails(req, res, next) {
-  projectLib.updateProjectOfInterviewee(
-    req.user._id,
-    req.body.project,
-    function(errInUpdation, updatedInstance) {
-      if (errInUpdation) {
-        res.status(500).json(errInUpdation);
-      } else {
-        res.status(200).json(updatedInstance);
+  if (req.body.hasOwnProperty('project') == false) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert projects details for project data inside object of project property',
+      errorDetail: 'There exists no project property for usage',
+    };
+    res.status(500).json(err);
+  } else if (req.body.project.hasOwnProperty('_id') == false) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert _id property inside requested Objects project property to update details of a project',
+      errorDetail:
+        'There exists no _id property inside project object for usage',
+    };
+    res.status(500).json(err);
+  } else if (req.body.project._id === '') {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg: 'Insert _id property inside project to update any stored data',
+      errorDetail:
+        '_id property is empty and doesnot contain any ObjectId string with a type as STRING',
+    };
+    res.status(500).json(err);
+  } else if (
+    req.body.project.hasOwnProperty('title') == false ||
+    req.body.project.hasOwnProperty('description') == false
+  ) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert a title and description field(required) inside requested Objects project property',
+      errorDetail:
+        'There exists no title or description or both property inside project object for usage',
+    };
+    res.status(500).json(err);
+  } else if (
+    req.body.project.title == '' ||
+    req.body.project.description == ''
+  ) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert a title and description field(required) inside requested Objects project property',
+      errorDetail:
+        'The title ot description or both fields are empty (Insert a specific different title from other projects title and a brief specific description of your project)',
+    };
+    res.status(500).json(err);
+  } else if (
+    typeof req.body.project.description != 'string' ||
+    typeof req.body.project.title != 'string'
+  ) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg: 'Insert correct values for property to be stored',
+      errorDetail:
+        'There exists some incorrect value for some property inside object requested',
+    };
+    res.status(500).json(err);
+  } else {
+    projectLib.updateProjectOfInterviewee(
+      req.user._id,
+      req.body.project,
+      function(errInUpdation, updatedInstance) {
+        if (errInUpdation) {
+          res.status(500).json(errInUpdation);
+        } else {
+          res.status(200).json(updatedInstance);
+        }
       }
-    }
-  );
+    );
+  }
 }
 
 /**
@@ -49,17 +157,43 @@ function updateProjectDetails(req, res, next) {
  * @param {Function} next Function to pass control to the next middleware
  */
 function deleteProjectDetails(req, res, next) {
-  projectLib.deleteProjectOfInterviewee(
-    req.user._id,
-    req.body.project._id,
-    function(errInUpdation, updatedInstance) {
-      if (errInUpdation) {
-        res.status(500).json(errInUpdation);
-      } else {
-        res.status(200).json(updatedInstance);
+  if (req.body.hasOwnProperty('project') == false) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg: 'Insert _id detail inside object of project property',
+      errorDetail: 'There exists no project property for usage',
+    };
+    res.status(500).json(err);
+  } else if (req.body.project.hasOwnProperty('_id') == false) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert _id property inside requested Objects project property to delete details of a project',
+      errorDetail:
+        'There exists no _id property inside project object for usage',
+    };
+    res.status(500).json(err);
+  } else if (req.body.project._id === '') {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg: 'Insert _id property inside project to delete any stored data',
+      errorDetail:
+        '_id property is empty and doesnot contain any ObjectId string with a type as STRING',
+    };
+    res.status(500).json(err);
+  } else {
+    projectLib.deleteProjectOfInterviewee(
+      req.user._id,
+      req.body.project._id,
+      function(errInUpdation, updatedInstance) {
+        if (errInUpdation) {
+          res.status(500).json(errInUpdation);
+        } else {
+          res.status(200).json(updatedInstance);
+        }
       }
-    }
-  );
+    );
+  }
 }
 
 router.post('/', addProjectDetails);
--- app/controllers/interviewee/profile/test.js
+++ app/controllers/interviewee/profile/test.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const testLib = require('../../../../lib/interviewee/profile/test');
+const CONSTANTS = require('../../../../config/constants');
 
 const router = express.Router();
 
@@ -10,16 +11,56 @@ const router = express.Router();
  * @param {Function} next Function to pass control to the next middleware
  */
 function addTestDetails(req, res, next) {
-  testLib.addTestOfInterviewee(req.user._id, req.body.test, function(
-    errInUpdation,
-    updatedInstance
+  if (req.body.hasOwnProperty('test') == false) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg: 'Insert tests details for tests data inside object of test property',
+      errorDetail: 'There exists no test property for usage',
+    };
+    res.status(500).json(err);
+  } else if (
+    req.body.test.hasOwnProperty('name') == false ||
+    req.body.test.hasOwnProperty('score') == false
   ) {
-    if (errInUpdation) {
-      res.status(500).json(errInUpdation);
-    } else {
-      res.status(200).json(updatedInstance);
-    }
-  });
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert name and score field(required) inside requested Objects test property',
+      errorDetail:
+        'There exists no property either name or score or both inside test object for usage',
+    };
+    res.status(500).json(err);
+  } else if (req.body.test.name === '' || req.body.test.score === '') {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert name and score field(required) inside requested Objects test property',
+      errorDetail: 'name or score or both fields are empty',
+    };
+    res.status(500).json(err);
+  } else if (
+    typeof req.body.test.name != 'string' ||
+    typeof req.body.test.score != 'string'
+  ) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg: 'Insert correct values for property to be stored',
+      errorDetail:
+        'There exists some incorrect value for some property inside object requested',
+    };
+    res.status(500).json(err);
+  } else {
+    testLib.addTestOfInterviewee(req.user._id, req.body.test, function(
+      errInUpdation,
+      updatedInstance
+    ) {
+      if (errInUpdation) {
+        res.status(500).json(errInUpdation);
+      } else {
+        res.status(200).json(updatedInstance);
+      }
+    });
+  }
 }
 
 /**
@@ -29,16 +70,72 @@ function addTestDetails(req, res, next) {
  * @param {Function} next Function to pass control to the next middleware
  */
 function updateTestDetails(req, res, next) {
-  testLib.updateTestOfInterviewee(req.user._id, req.body.test, function(
-    errInUpdation,
-    updatedInstance
+  if (req.body.hasOwnProperty('test') == false) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg: 'Insert tests details for tests data inside object of test property',
+      errorDetail: 'There exists no test property for usage',
+    };
+    res.status(500).json(err);
+  } else if (req.body.test.hasOwnProperty('_id') == false) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert _id property inside requested Objects test property to update details of a test',
+      errorDetail: 'There exists no _id property inside test object for usage',
+    };
+    res.status(500).json(err);
+  } else if (req.body.test._id === '') {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg: 'Insert _id property inside test to update any stored data',
+      errorDetail:
+        '_id property is empty and doesnot contain any ObjectId string with a type as STRING',
+    };
+    res.status(500).json(err);
+  } else if (
+    req.body.test.hasOwnProperty('name') == false ||
+    req.body.test.hasOwnProperty('score') == false
   ) {
-    if (errInUpdation) {
-      res.status(500).json(errInUpdation);
-    } else {
-      res.status(200).json(updatedInstance);
-    }
-  });
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert name and score field(required) inside requested Objects test property',
+      errorDetail:
+        'There exists no property either name or score or both inside test object for usage',
+    };
+    res.status(500).json(err);
+  } else if (req.body.test.name === '' || req.body.test.score === '') {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert name and score field(required) inside requested Objects test property',
+      errorDetail: 'name or score or both fields are empty',
+    };
+    res.status(500).json(err);
+  } else if (
+    typeof req.body.test.name != 'string' ||
+    typeof req.body.test.score != 'string'
+  ) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg: 'Insert correct values for property to be stored',
+      errorDetail:
+        'There exists some incorrect value for some property inside object requested',
+    };
+    res.status(500).json(err);
+  } else {
+    testLib.updateTestOfInterviewee(req.user._id, req.body.test, function(
+      errInUpdation,
+      updatedInstance
+    ) {
+      if (errInUpdation) {
+        res.status(500).json(errInUpdation);
+      } else {
+        res.status(200).json(updatedInstance);
+      }
+    });
+  }
 }
 
 /**
@@ -48,16 +145,41 @@ function updateTestDetails(req, res, next) {
  * @param {Function} next Function to pass control to the next middleware
  */
 function deleteTestDetails(req, res, next) {
-  testLib.deleteTestOfInterviewee(req.user._id, req.body.test._id, function(
-    errInUpdation,
-    updatedInstance
-  ) {
-    if (errInUpdation) {
-      res.status(500).json(errInUpdation);
-    } else {
-      res.status(200).json(updatedInstance);
-    }
-  });
+  if (req.body.hasOwnProperty('test') == false) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg: 'Insert tests details for tests data inside object of test property',
+      errorDetail: 'There exists no test property for usage',
+    };
+    res.status(500).json(err);
+  } else if (req.body.test.hasOwnProperty('_id') == false) {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg:
+        'Insert _id property inside requested Objects test property to delete details of a test',
+      errorDetail: 'There exists no _id property inside test object for usage',
+    };
+    res.status(500).json(err);
+  } else if (req.body.test._id === '') {
+    let err = {
+      type: CONSTANTS.ERROR_TYPES.INCORRECT_PAYLOAD,
+      msg: 'Insert _id property inside test to delete any stored data',
+      errorDetail:
+        '_id property is empty and doesnot contain any ObjectId string with a type as STRING',
+    };
+    res.status(500).json(err);
+  } else {
+    testLib.deleteTestOfInterviewee(req.user._id, req.body.test._id, function(
+      errInUpdation,
+      updatedInstance
+    ) {
+      if (errInUpdation) {
+        res.status(500).json(errInUpdation);
+      } else {
+        res.status(200).json(updatedInstance);
+      }
+    });
+  }
 }
 
 router.post('/', addTestDetails);
