7cfba973f5ad135561657fe4cd72dd55dc984ab1
saurabh2044 <24.sinha.shubham@gmail.com>
2018-03-26 18:43:10 +0530

    Interview Page skeleton

---
+++ client/src/components/interview/Timer.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { connect } from "react-redux";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Avatar from "material-ui/Avatar";
+import Chip from "material-ui/Chip";
+import { blue300, red300, indigo900, red900 } from "material-ui/styles/colors";
+
+const styles = {
+  chip: {
+    margin: 4,
+    fontSize: "27px"
+  },
+  wrapper: {
+    display: "flex",
+    flexWrap: "wrap"
+  }
+};
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: {}, seconds: 120 };
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+  }
+
+  secondsToTime(secs) {
+    let hours = Math.floor(secs / (60 * 60));
+
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    let obj = {
+      h: hours,
+      m: minutes,
+      s: seconds
+    };
+    return obj;
+  }
+
+  componentDidMount() {
+    let timeLeftVar = this.secondsToTime(this.state.seconds);
+    this.setState({ time: timeLeftVar });
+    this.startTimer();
+  }
+
+  startTimer() {
+    if (this.timer == 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+
+  restartTimer() {
+    this.timer = setInterval(this.countDown, 1000);
+  }
+
+  countDown() {
+    // Remove one second, set state so a re-render happens.
+    let seconds = this.state.seconds - 1;
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds
+    });
+
+    // Check if we're at zero.
+    if (seconds == 0) {
+      clearInterval(this.timer);
+      this.props.endCallback();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider muiTheme={this.props.muiTheme}>
+          <div style={styles.wrapper}>
+            <Chip
+              backgroundColor={
+                this.state.time.m == 0 && this.state.time.s < 11
+                  ? red300
+                  : blue300
+              }
+              style={styles.chip}
+            >
+              <Avatar
+                icon={<i className="far fa-clock" />}
+                backgroundColor={
+                  this.state.time.m == 0 && this.state.time.s < 11
+                    ? red900
+                    : indigo900
+                }
+              />
+              {this.state.time.m} : {this.state.time.s}
+            </Chip>
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ company }) {
+  return { company };
+}
+export default connect(mapStateToProps)(Timer);
--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { connect } from 'react-redux';
-import BusyIndicator from '../../components/common/busyIndicator';
-import ReactCardFlip from '../../components/login/ReactCardFlip';
-import LoginPage from '../../components/login/Login';
-import Register from '../../components/login/Register';
-import './index.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { lightBlue100 } from 'material-ui/styles/colors';
-let imgUrl="/Assets/Hire.png"
+import React from "react";
+import ReactDOM from "react-dom";
+import { connect } from "react-redux";
+import BusyIndicator from "../../components/common/busyIndicator";
+import ReactCardFlip from "../../components/login/ReactCardFlip";
+import LoginPage from "../../components/login/Login";
+import Register from "../../components/login/Register";
+import "./index.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import { lightBlue100 } from "material-ui/styles/colors";
+let imgUrl = "/Assets/Hire.png";
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isFlipped: false,
+      isFlipped: false
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -26,54 +26,65 @@ class Login extends React.Component {
 
   render() {
     return (
-      <MuiThemeProvider muiTheme={this.props.muiTheme} >
-      <div >
-        <div className="columns " style={{marginTop: 40,backgroundColor: lightBlue100}} >
-          <div className="column is-7">
+      <MuiThemeProvider muiTheme={this.props.muiTheme}>
+        <div>
+          <div
+            className="columns "
+            style={{ marginTop: 40, backgroundColor: lightBlue100 }}
+          >
+            <div className="column is-7">
+              <img
+                src="./Assets/Hire.png"
+                alt="SmartHyre"
+                width="600"
+                height="15"
+                border="5px #ff0000"
+              />
+            </div>
+            <div className="column is-5">
+              <div
+                className="container"
+                style={{ maxWidth: 400, marginTop: 15 }}
+              >
+                <ReactCardFlip isFlipped={this.state.isFlipped}>
+                  <div key="front">
+                    <h1 className="card-header-title title is-2 is-centered ">
+                      Login
+                    </h1>
 
-            <img src="./Assets/Hire.png" alt="SmartHyre" width="600" height="15" border="5px #ff0000" />
+                    <LoginPage className="card-content" />
+                    <br />
+                    <a
+                      className="button is-primary card-footer"
+                      onClick={this.handleClick}
+                    >
+                      New here?
+                    </a>
+                    <br />
+                  </div>
 
+                  <div key="back">
+                    <h1 className="card-header-title title is-2 is-centered ">
+                      Register
+                    </h1>
 
-          </div>
-          <div className="column is-5">
-            <div className="container" style={{ maxWidth: 400, marginTop: 15 }}>
-              <ReactCardFlip isFlipped={this.state.isFlipped}>
-                <div key="front" >
-                  <h1 className="card-header-title title is-2 is-centered ">Login</h1>
-
-                  <LoginPage className="card-content" />
-                  <br />
-                  <a
-                    className="button is-primary card-footer"
-                    onClick={this.handleClick}
-                  >
-                    New here?
-            </a>
-                  <br />
-                </div>
-
-                <div key="back" >
-                  <h1 className="card-header-title title is-2 is-centered ">
-                    Register
-            </h1>
-
-                  <Register className="card-content" />
-                  <br />
-                  <br />
-                  <a
-                    className="button is-primary card-footer"
-                    onClick={this.handleClick}
-                  >
-                    Been here! Sign in
-            </a>
-                  <br />
-                </div>
-              </ReactCardFlip>
-              {this.props.loading.isloading ? <BusyIndicator /> : null}
+                    <Register className="card-content" />
+                    <br />
+                    <br />
+                    <a
+                      className="button is-primary card-footer"
+                      onClick={this.handleClick}
+                    >
+                      Been here! Sign in
+                    </a>
+                    <br />
+                  </div>
+                </ReactCardFlip>
+                {this.props.loading.isloading ? <BusyIndicator /> : null}
+              </div>
             </div>
           </div>
         </div>
-</div>
       </MuiThemeProvider>
     );
   }
