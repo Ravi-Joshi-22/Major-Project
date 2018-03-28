43b8f45680cf819886e85a902ecc2891934c545c
shreya <shreyajhunjhunwala7@gmail.com>
2018-03-28 12:46:30 +0530

    change in dev.js file

--- config/dev.js
+++ config/dev.js
@@ -4,8 +4,7 @@
  */
 
 module.exports = {
-  // MONGO_DB_URI: 'mongodb://localhost:27017/smartHyre',
-  MONGO_DB_URI: 'mongodb://shreya:shreya@ds249718.mlab.com:49718/smarthyre',
+  MONGO_DB_URI: 'mongodb://localhost:27017/smartHyre',
   COOKIE_KEY: 'sygfdjsnfjdsffrmtniioscnaqiwi2774',
   MAIL: {
     SERVICE: 'gmail',
