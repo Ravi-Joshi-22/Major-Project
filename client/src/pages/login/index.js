9155832af1eaa4bf5ce5a6870072915c7cdbb0b2
saurabh2044 <24.sinha.shubham@gmail.com>
2018-04-03 03:49:16 +0530

    Login page upgraded

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -28,59 +28,81 @@ class Login extends React.Component {
     return (
       <MuiThemeProvider muiTheme={this.props.muiTheme}>
         <div>
-          <div
-            className="columns "
-            style={{ marginTop: 40, backgroundColor: lightBlue100 }}
-          >
-            <div className="column is-7">
-              <img
-                src="./Assets/Hire.png"
-                alt="SmartHyre"
-                width="600"
-                height="15"
-                border="5px #ff0000"
-              />
-            </div>
-            <div className="column is-5">
-              <div
-                className="container"
-                style={{ maxWidth: 400, marginTop: 15 }}
-              >
-                <ReactCardFlip isFlipped={this.state.isFlipped}>
-                  <div key="front">
-                    <h1 className="card-header-title title is-2 is-centered ">
-                      Login
-                    </h1>
+          <video autoPlay muted loop id="myVideo">
+            <source src=".\Assets\BGVid1.mp4" type="video/mp4" />
+            Your browser does not support HTML5 video.
+          </video>
+          <div className="content">
+            <div
+              className="columns "
+              style={{
+                backgroundColor: "rgba(189, 195, 199, 0.5)",
+                margin: "auto",
+                marginTop: "10%",
+                maxWidth: "80%"
+              }}
+            >
+              <div className="column is-6" style={{ position: "relative" }}>
+                <img
+                  src="./Assets/logoTrans.svg"
+                  alt="SmartHyre"
+                  style={{
+                    position: "absolute",
+                    background: "rgba(255,255,255,0.0)",
+                    width: "50%",
+                    height: "auto",
+                    margin: '5%'
+                  }}
+                />
+                <img
+                  src="./Assets/Hire.png"
+                  alt="SmartHyre"
+                  width="600"
+                  height="15"
+                  border="5px #ff0000"
+                />
+              </div>
+              <div className="column is-6">
+                <div
+                  className="container"
+                  style={{ maxWidth: "80%", marginTop: 15 }}
+                >
+                  <ReactCardFlip isFlipped={this.state.isFlipped}>
+                    <div key="front">
+                      <h1 className="card-header-title title is-2 is-centered ">
+                        Login
+                      </h1>
 
-                    <LoginPage className="card-content" />
-                    <br />
-                    <a
-                      className="button is-primary card-footer"
-                      onClick={this.handleClick}
-                    >
-                      New here?
-                    </a>
-                    <br />
-                  </div>
+                      <LoginPage className="card-content" />
+                      <br />
+                      <a
+                        className="button is-primary card-footer"
+                        onClick={this.handleClick}
+                      >
+                        New here?
+                      </a>
+                      <br />
+                    </div>
 
-                  <div key="back">
-                    <h1 className="card-header-title title is-2 is-centered ">
-                      Register
-                    </h1>
+                    <div key="back">
+                      <h1 className="card-header-title title is-2 is-centered ">
+                        Register
+                      </h1>
 
-                    <Register className="card-content" />
-                    <br />
-                    <br />
-                    <a
-                      className="button is-primary card-footer"
-                      onClick={this.handleClick}
-                    >
-                      Been here! Sign in
-                    </a>
-                    <br />
-                  </div>
-                </ReactCardFlip>
-                {this.props.loading.isloading ? <BusyIndicator /> : null}
+                      <Register className="card-content" />
+                      <br />
+                      <br />
+                      <a
+                        className="button is-primary card-footer"
+                        onClick={this.handleClick}
+                      >
+                        Been here! Sign in
+                      </a>
+                      <br />
+                    </div>
+                  </ReactCardFlip>
+                  {this.props.loading.isloading ? <BusyIndicator /> : null}
+                </div>
               </div>
             </div>
           </div>
