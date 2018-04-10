92c013a515f715ff57bac0eac2a8c4c0ac387211
Jo-ravar <ravijoshi0996@gmail.com>
2018-04-10 11:29:51 +0530

    Changed width

--- client/src/components/companyDashboard/companyResult/openingResultCard.js
+++ client/src/components/companyDashboard/companyResult/openingResultCard.js
@@ -26,14 +26,32 @@ class OpeningResultCard extends React.Component {
     if (this.props.results && this.props.results.length > 0) {
       const resultData = this.props.results.map((eachResult, key) => {
         return (
-          <TableRow>
-            <TableRowColumn>
-              {eachResult.user_id.first_name +
+          <TableRow style={{ width: '100%' }}>
+            <TableRowColumn style={{ width: '33%' }}>
+              {eachResult.user_id.first_name.toUpperCase() +
                 ' ' +
-                eachResult.user_id.last_name}
+                eachResult.user_id.last_name.toUpperCase()}
             </TableRowColumn>
-            <TableRowColumn>{eachResult.score}</TableRowColumn>
-            <TableRowColumn>{eachResult.interview_status}</TableRowColumn>
+            <TableRowColumn style={{ width: '33%' }}>
+              {eachResult.score.toFixed(2)}
+            </TableRowColumn>
+            {eachResult.interview_status === 'selected' ? (
+              <TableRowColumn style={{ color: 'green', width: '33%' }}>
+                {eachResult.interview_status.toUpperCase()}
+              </TableRowColumn>
+            ) : eachResult.interview_status === 'applied' ? (
+              <TableRowColumn style={{ color: 'yellow', width: '33%' }}>
+                {eachResult.interview_status.toUpperCase()}
+              </TableRowColumn>
+            ) : eachResult.interview_status === 'rejected' ? (
+              <TableRowColumn style={{ color: 'red', width: '33%' }}>
+                {eachResult.interview_status.toUpperCase()}
+              </TableRowColumn>
+            ) : (
+              <TableRowColumn style={{ color: 'blue', width: '33%' }}>
+                {eachResult.interview_status.toUpperCase()}
+              </TableRowColumn>
+            )}
           </TableRow>
         );
       });
@@ -44,23 +62,25 @@ class OpeningResultCard extends React.Component {
   }
 
   render() {
-    const fields = ['NAME OF INTERVIEWEE', 'SCORE OBTAINED'];
+    const fields = ['NAME OF INTERVIEWEE', 'SCORE OBTAINED', 'STATUS'];
     return (
       <MuiThemeProvider muiTheme={this.props.muiTheme}>
         <Card style={{ padding: 25, margin: 20 }}>
           <CardTitle title="Interviewee Details" />
           <CardText style={{ padding: 2 }}>
-            <Table displayRowCheckbox={false} selectable={false}>
-              <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+            <Table selectable={false}>
+              <TableHeader displaySelectAll={false} adjustForCheckbox={true}>
                 <TableHeaderColumn>
                   {fields.map((eachField, key) => (
-                    <TableRowColumn style={{ width: '15%' }}>
+                    <TableRowColumn style={{ width: '33%' }}>
                       {eachField}
                     </TableRowColumn>
                   ))}
                 </TableHeaderColumn>
               </TableHeader>
-              <TableBody>{this.renderResults()}</TableBody>
+              <TableBody displayRowCheckbox={false} adjustForCheckbox={true}>
+                {this.renderResults()}
+              </TableBody>
             </Table>
           </CardText>
         </Card>
