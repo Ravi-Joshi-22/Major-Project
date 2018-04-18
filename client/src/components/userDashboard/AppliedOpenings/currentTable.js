13ecde6fc44fa160310a1f76f50ecbbad405caf0
shreyajhunjhunwala7@gmail.com <shivani3608@gmail.com>
2018-04-18 23:28:32 +0530

    User Tables Enhanced

--- client/src/components/userDashboard/AppliedOpenings/currentTable.js
+++ client/src/components/userDashboard/AppliedOpenings/currentTable.js
@@ -1,13 +1,11 @@
-import React from "react";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import * as actions from "../../../actions/app";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import IconButton from "material-ui/IconButton/IconButton";
-import ClearIcon from "material-ui/svg-icons/content/clear";
-import MoreIcon from "material-ui/svg-icons/hardware/keyboard-arrow-right";
-import GiveIcon from "material-ui/svg-icons/hardware/computer";
-import FlatButton from "material-ui/FlatButton";
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../../actions/app';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import IconButton from 'material-ui/IconButton/IconButton';
+import GiveIcon from 'material-ui/svg-icons/hardware/computer';
+import FlatButton from 'material-ui/FlatButton';
 
 class CurrentTable extends React.Component {
   constructor(props) {
@@ -19,28 +17,27 @@ class CurrentTable extends React.Component {
     const dateStr = new Date(date);
     const sDateStr =
       dateStr.getDate() +
-      "- " +
+      '- ' +
       (dateStr.getMonth() + 1) +
-      "- " +
+      '- ' +
       dateStr.getFullYear();
     return sDateStr;
   }
 
   startInterview(trackId) {
     this.props.storeTrackId(trackId);
-    this.props.history.push("/Interview");
+    this.props.history.push('/Interview');
   }
 
   render() {
     const fieldsC = [
-      "COMPANY NAME",
-      "PROFILE",
-      "LOCATION",
-      "EXPERIENCE",
-      "START DATE",
-      "END DATE",
-      "MORE",
-      "GIVE INTERVIEW"
+      'COMPANY NAME',
+      'PROFILE',
+      'LOCATION',
+      'EXPERIENCE',
+      'START DATE',
+      'END DATE',
+      'GIVE INTERVIEW',
     ];
 
     return (
@@ -49,7 +46,7 @@ class CurrentTable extends React.Component {
           <thead>
             <tr>
               {fieldsC.map((eachField, key) => (
-                <th key={key} style={{ width: "15%" }}>
+                <th key={key} style={{ width: '15%' }}>
                   {eachField}
                 </th>
               ))}
@@ -58,27 +55,22 @@ class CurrentTable extends React.Component {
           <tbody>
             {this.props.interviewData.map((eachOpening, key) => (
               <tr key={key}>
-                <td style={{ width: "15%" }}>
+                <td style={{ width: '15%' }}>
                   {eachOpening.company_id.company_name}
                 </td>
-                <td style={{ width: "20%" }}>{eachOpening.position}</td>
-                <td style={{ width: "10%" }}>{eachOpening.location}</td>
-                <td style={{ width: "15%" }}>
-                  {eachOpening.experience_min} - {eachOpening.experience_max}{" "}
+                <td style={{ width: '20%' }}>{eachOpening.position}</td>
+                <td style={{ width: '10%' }}>{eachOpening.location}</td>
+                <td style={{ width: '15%' }}>
+                  {eachOpening.experience_min} - {eachOpening.experience_max}{' '}
                   yrs
                 </td>
-                <td style={{ width: "10%" }}>
+                <td style={{ width: '10%' }}>
                   {this.getDateStr(eachOpening.start_date)}
                 </td>
-                <td style={{ width: "10%" }}>
+                <td style={{ width: '10%' }}>
                   {this.getDateStr(eachOpening.end_date)}
                 </td>
-                <td style={{ width: "5%" }}>
-                  <IconButton>
-                    <MoreIcon />
-                  </IconButton>
-                </td>
-                <td style={{ width: "10%" }}>
+                <td style={{ width: '10%' }}>
                   <FlatButton
                     label="GIVE"
                     primary={true}
@@ -87,11 +79,6 @@ class CurrentTable extends React.Component {
                       this.startInterview(eachOpening.interviewees[0])
                     }
                   />
-                </td>
-                <td style={{ width: "5%" }}>
-                  <IconButton>
-                    <ClearIcon />
-                  </IconButton>
                 </td>
               </tr>
             ))}
